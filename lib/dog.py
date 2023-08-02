9e26b747f6dd92bb1cc2a59bb347b926262599e9
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-08-02 10:17:22 -0600

    Implements database functionality with SQLAlchemy help!

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,42 @@
 from models import Dog
+import os
 
-def create_table(base):
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+from models import Base, Dog
+from testing.conftest import db_dir, SQLITE_URL
+
+
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
     pass
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog)
+    return dogs
+
 
 def find_by_name(session, name):
-    pass
+    named_dogs = session.query(Dog).filter(Dog.name == name).all()
+    # We get a list of tuples.  Send back the 0th element.
+    return named_dogs[0]
 
 def find_by_id(session, id):
-    pass
+    id_dog = session.query(Dog).filter(Dog.id == id).all()
+    return id_dog[0]
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    desired_dog = session.query(Dog).filter(Dog.name == name, Dog.breed == breed)
+    return desired_dog[0]
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+
+
